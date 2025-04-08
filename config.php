3938ae019f8dadf65f1eb0c440fa6f9803cedf13
eunhong@amazon.com <eunhong@amazon.com>
2025-04-09 08:26:44 +0900

    DB修正

--- config.php
+++ config.php
@@ -1,5 +1,5 @@
 <?php
-define('DB_SERVER', 'web-app-db.cnkowo6samsk.ap-northeast-1.rds.amazonaws.com');
+define('DB_SERVER', 'web-app-db.cfi6e4ooqkhe.ap-northeast-3.rds.amazonaws.com');
 define('DB_USERNAME', 'admin');
 define('DB_PASSWORD', 'AWSeunhong1!');
 define('DB_DATABASE', 'myapp');
