387d3fad13b6082ff876035f8412afad43b19a00
eunhong99 <eunhong@amazon.com>
2025-04-08 16:31:52 +0900

    Update database configuration

--- config.php
+++ config.php
@@ -2,5 +2,5 @@
 define('DB_SERVER', 'web-app-db.cnkowo6samsk.ap-northeast-1.rds.amazonaws.com');
 define('DB_USERNAME', 'admin');
 define('DB_PASSWORD', 'AWSeunhong1!');
-define('DB_DATABASE', 'webappdb');
+define('DB_DATABASE', 'web-app-db');
 ?>
